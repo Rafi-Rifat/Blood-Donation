d434b32b89fc901b438148e1574dcfd9054c0f8e
Shawn-33 <mohammadshawn12003033@gmail.com>
2023-10-31 16:37:57 +0600

    ssssssss

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import 'package:auth_app/Work/DonerData.dart';
 import 'package:flutter/material.dart';
 
@@ -6,22 +5,13 @@ class ProfilePage extends StatelessWidget {
   final DonerData don;
   ProfilePage({required this.don});
 
-=======
-import 'package:flutter/material.dart';
-
-class ProfilePage extends StatelessWidget {
->>>>>>> origin/master
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Profile'),
       ),
-<<<<<<< HEAD
       body: Center(
-=======
-      body: const Center(
->>>>>>> origin/master
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
@@ -32,11 +22,7 @@ class ProfilePage extends StatelessWidget {
             ),
             SizedBox(height: 20),
             Text(
-<<<<<<< HEAD
               don.name, // Use the name from the 'don' object
-=======
-              'User Name',
->>>>>>> origin/master
               style: TextStyle(
                 fontSize: 24,
                 fontWeight: FontWeight.bold,
@@ -44,22 +30,14 @@ class ProfilePage extends StatelessWidget {
             ),
             SizedBox(height: 10),
             Text(
-<<<<<<< HEAD
               'Email: ${don.email}', // Use the email from the 'don' object
-=======
-              'Email: user@example.com',
->>>>>>> origin/master
               style: TextStyle(
                 fontSize: 18,
               ),
             ),
             SizedBox(height: 10),
             Text(
-<<<<<<< HEAD
               'Blood Type: ${don.bl}', // Use the blood type from the 'don' object
-=======
-              'Blood Group: AB+',
->>>>>>> origin/master
               style: TextStyle(
                 fontSize: 18,
               ),
