48a3e0dcbc2e0e4e156300f4f550eb32b0f56a66
Rafi-Rifat <rafialsaadrifat@gmail.com>
2023-11-02 10:04:23 +0600

    r

--- lib/screens/post1.dart
+++ lib/screens/post1.dart
@@ -1,3 +1,29 @@
+// import 'package:auth_app/Work/PostData.dart';
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+
+// class PostCard extends StatelessWidget {
+//   final postData pos;
+
+//   const PostCard({Key? key, required this.pos}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Card(
+//       child: ListTile(
+//         title: Column(
+//           crossAxisAlignment: CrossAxisAlignment.start,
+//           children: [
+//             Text(pos.name),
+//             Text(pos.date),
+//             SizedBox(height: 10),
+//             Text(pos.postText),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
 import 'package:auth_app/Work/PostData.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -10,14 +36,35 @@ class PostCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
+      elevation: 5, // Add elevation for a shadow effect.
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0), // Add rounded corners.
+      ),
       child: ListTile(
+        contentPadding: EdgeInsets.all(16.0), // Add padding to ListTile.
         title: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(pos.name),
-            Text(pos.date),
+            Text(
+              pos.name,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+            ),
+            // Text(
+            //   pos.date,
+            //   style: TextStyle(
+            //     color: Colors.grey,
+            //   ),
+            // ),
             SizedBox(height: 10),
-            Text(pos.postText),
+            Text(
+              pos.postText,
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
           ],
         ),
       ),
